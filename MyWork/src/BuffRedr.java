9a9b2dbdb29ff875b25b319b7510e07ed0c51ac5
SHASHANK B K <SHASHANK B K@DESKTOP-TP9VLE9>
2020-06-02 14:14:47 +0530

    committed my first java project

---
+++ MyWork/src/BuffRedr.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BuffRedr {
+
+	public static void main(String[] args) throws IOException {
+		
+		
+		System.out.println("enter the numb");
+		
+		InputStreamReader is=new InputStreamReader(System.in);
+		BufferedReader br=new BufferedReader(is);
+		
+		int n=Integer.parseInt(br.readLine());
+		
+		System.out.println(n);
+		
+		
+		
+		
+
+	}
+
+}
