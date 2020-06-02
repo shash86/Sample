63b54bfe4095fc186a135c4ea7b8ee4b9fab8348
SHASHANK B K <SHASHANK B K@DESKTOP-TP9VLE9>
2020-06-02 14:29:04 +0530

    implemented Example class

--- MyWork/src/com/shashank/example/Example.java
+++ MyWork/src/com/shashank/example/Example.java
@@ -3,8 +3,9 @@ package com.shashank.example;
 public class Example {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.out.println("hello world");
+		
 	}
 
 }
